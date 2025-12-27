da27a290bfb460dc8ca742559ae93a4fa40afba3
thaibao-15 <phamthaibao2005thd@gmail.com>
2025-12-27 20:55:40 +0700

    return

---
+++ fontend/src/configurations/configuration.js
@@ -0,0 +1,12 @@
+export const CONFIG = {
+    API_GATEWAY: "http://localhost:8080",
+};
+
+export const API = {
+    LOGIN: "/identity/auth/token",
+    PRODUCT: "/identity/products",
+    CART: "",
+    CART_ITEM: "/identity/cartItems",
+    CHECKOUT: "/identity/checkout",
+    ORDER: ""
+}
